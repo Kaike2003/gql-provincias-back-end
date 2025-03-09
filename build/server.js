620944b93a1b79476db54a5a243879de096c76ca
Kaike Bartolomeu <102388705+Kaike2003@users.noreply.github.com>
2025-03-09 16:06:26 +0100

    atualizado o script

---
+++ build/server.js
@@ -0,0 +1,39 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const merge_1 = require("@graphql-tools/merge");
+const load_files_1 = require("@graphql-tools/load-files");
+const node_path_1 = require("node:path");
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const allTypes = (0, load_files_1.loadFilesSync)((0, node_path_1.join)(__dirname, "graphql", "**", "*.gql"));
+const allResolvers = (0, load_files_1.loadFilesSync)((0, node_path_1.join)(__dirname, "graphql", "**", "resolvers.ts"));
+const typeDefs = (0, merge_1.mergeTypeDefs)(allTypes);
+const resolvers = (0, merge_1.mergeResolvers)(allResolvers);
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const server = new apollo_server_express_1.ApolloServer({ typeDefs, resolvers });
+    yield server.start();
+    const app = (0, express_1.default)();
+    app.use(express_1.default.json());
+    app.use(express_1.default.urlencoded());
+    app.use((0, cors_1.default)());
+    server.applyMiddleware({ app });
+    app.listen({ port: process.env.PORT }, () => {
+        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+    });
+});
+main();
